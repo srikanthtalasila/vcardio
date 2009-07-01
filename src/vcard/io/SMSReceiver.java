66a347a16fb3e311f63ba974870e88b42c6f3af9
francisdb <francisdb@localhost>
2009-07-01 00:34:52 -0400

    Added initial support for SMS

---
+++ src/vcard/io/SMSReceiver.java
@@ -0,0 +1,83 @@
+package vcard.io;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Contacts.People;
+import android.telephony.gsm.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * The class is called when SMS is received.
+ * 
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+    /**
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+
+        Object messages[] = (Object[]) bundle.get("pdus");
+        SmsMessage smsMessage[] = new SmsMessage[messages.length];
+        for (int n = 0; n < messages.length; n++) {
+            smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+        }
+
+        String body;
+        for(SmsMessage message:smsMessage){
+        	body = message.getMessageBody();
+        	if(isVCard(body)){
+        		Uri person = readCard(context, body);
+        		
+        		notify(context, "Adding contact data received from " + message.getOriginatingAddress(), person.toString());
+        	}
+        }
+    }
+    
+	private static final int HELLO_ID = 1;
+    
+    private void notify(final Context context, final String title, final String text){
+    	String ns = Context.NOTIFICATION_SERVICE;
+    	NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(ns);
+    	int icon = R.drawable.icon;
+    	CharSequence tickerText = "New contact received";
+    	long when = System.currentTimeMillis();
+
+    	Notification notification = new Notification(icon, tickerText, when);
+    	
+    	CharSequence contentTitle = title;
+    	CharSequence contentText = text;
+    	Intent notificationIntent = new Intent(context, SMSReceiver.class);
+    	PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+    	notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+    	mNotificationManager.notify(HELLO_ID, notification);
+    }
+    
+    private boolean isVCard(final String content){
+    	return content.startsWith("BEGIN:VCARD");
+    }
+    
+    private Uri readCard(final Context context, final String vcard) {
+    	Contact contact = new Contact(vcard, null, null, null);
+    	long row = contact.addContact(context, 0, false);
+    	Uri myPerson = ContentUris.withAppendedId(People.CONTENT_URI, row);
+    	return myPerson;
+    }    
+    
+    private void showToUser(final Context context, final String message){
+    	 Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+         Log.i("test", message );
+         toast.show();
+    }
+}
